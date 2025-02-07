bedbf1cb382ae8c885ef43d03c7c2fc82ea66769
Mey <mey@lizainslie.dev>
2025-02-07 01:14:44 -0600

    feat(discord): embed url, dsl improvements & dsl example

--- src/main/kotlin/dev/lizainslie/cafemc/util/DiscordUtils.kt
+++ src/main/kotlin/dev/lizainslie/cafemc/util/DiscordUtils.kt
@@ -11,10 +11,11 @@ object DiscordUtils {
 }
 
 class EmbedBuilderDsl {
-    val embedBuilder = EmbedBuilder()
+    private val embedBuilder = EmbedBuilder()
 
     var title: String? = null
     var description: String? = null
+    var url: String? = null
     var thumbnail: String? = null
     var image: String? = null
     var timestamp: TemporalAccessor? = null
@@ -35,6 +36,10 @@ class EmbedBuilderDsl {
     
     fun author(block: AuthorBuilder.() -> Unit) {
         _author = AuthorBuilder().apply(block)
+    }
+    
+    fun author(name: String?, url: String? = null, iconUrl: String? = null) {
+        _author = AuthorBuilder(name, url, iconUrl)
     }
     
     data class FieldBuilder(
@@ -73,7 +78,7 @@ class EmbedBuilderDsl {
     }
     
     fun build() = embedBuilder.apply {
-        title?.let { setTitle(it) }
+        title?.let { setTitle(it, url) } // todo: maybe set the url differently?
         description?.let { setDescription(it) }
         thumbnail?.let { setThumbnail(it) }
         image?.let { setImage(it) }
@@ -95,5 +100,60 @@ class EmbedBuilderDsl {
     
     companion object {
         const val DEFAULT_COLOR = 536870911
+    }
+}
+
+@Suppress("unused")
+internal fun dslExample() {
+    val messageEmbed = DiscordUtils.buildEmbed {
+        // some basic embed properties
+        title = "Title"
+        url = "https://example.com"
+        description = "Description"
+        thumbnail = "https://example.com/thumbnail.png"
+        image = "https://example.com/image.png"
+        timestamp = java.time.Instant.now()
+        
+        
+        // to set the color...
+        color = 0xFFFFFF // you can use an int
+        color(Color.RED) // or a java.awt.Color
+        
+        
+        author {// you can add an author with a block
+            name = "Author"
+            url = "https://example.com/author"
+            iconUrl = "https://example.com/icon.png"
+        }
+        
+        // or a function
+        author("Author", "https://example.com/author", "https://example.com/icon.png")
+        
+        
+        // fields can be added in a few ways
+        
+        field { // you can use a block
+            name = "Block Field 1"
+            value = "Block Value 1"
+            // inline is false by default
+            inline = true
+        }
+        
+        // or you can use a function
+        field("Field 1", "Value 1")
+        field("Field 2", "Value 2", inline = true)
+        
+        // you can also add blank fields
+        // this is a bit of a hack but useful for formatting
+        blankField()
+        
+        
+        // you can also add a footer with a function
+        footer("Footer", "https://example.com/footer.png")
+        
+        footer { // or a block
+            text = "Footer"
+            iconUrl = "https://example.com/footer.png"
+        }
     }
 }
